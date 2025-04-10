bb60276e282cf03fd4f11a0b5f7f4aa4b521dc25
0zge-0zturk <113148241+0zge-0zturk@users.noreply.github.com>
2025-04-10 03:34:25 -0700

    Create math_utils.py

---
+++ math_utils.py
@@ -0,0 +1,20 @@
+def add(x,y):
+    return x + y
+
+def subtract(x,y):
+    return x - y
+
+def multiply(x,y):
+    return x * y
+
+def divide(x,y):
+    if y == 0:
+        print("Divisor cannot be zero")
+    else:
+        return x / y
+
+def power(x,y):
+    return x ** y
+
+def mod(x,y):
+    return x % y
